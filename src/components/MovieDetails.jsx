595f5c5cfe4e25466f5bb20806fdc1b31f153267
WaqarAhmad <technologymad3@gmail.com>
2023-10-05 15:40:37 +0500

    Final Commit

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -63,7 +63,7 @@ export function MovieDetails() {
     async function getMovieDetails() {
       setIsLoading(true);
       const res =
-        await fetch(`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}
+        await fetch(`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedID}
       `);
       const data = await res.json();
       setMovie(data);
